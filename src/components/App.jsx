c856277dd0bbc6346966f2a3655cc2bfb4c2fad3
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-07-02 11:11:30 +0300

    Fix mentor's comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Routes, Route, Navigate } from 'react-router-dom';
-// import { useState, useRef } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import useLocalStorage from 'hooks/useLocalStorage';
 import { lazy, Suspense } from 'react';
-import { ReactQueryDevtools } from 'react-query/devtools';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { light, dark } from '../themes';
 import { Container } from './App.styled';
@@ -13,16 +12,16 @@ import Appbar from './AppBar/Appbar';
 import ThreeDots from './Loaders/Loader';
 
 const HomePage = lazy(() =>
-  import('./HomePage' /* webpackChunkName: "home-page" */)
+  import('../pages/HomePage' /* webpackChunkName: "home-page" */)
 );
 const MoviesPage = lazy(() =>
-  import('./MoviesPage' /* webpackChunkName: "moviePage" */)
+  import('../pages/MoviesPage' /* webpackChunkName: "moviePage" */)
 );
 const MovieDetailsPage = lazy(() =>
-  import('./MovieDetailsPage' /* webpackChunkName: "movieDetailsPage" */)
+  import('../pages/MovieDetailsPage' /* webpackChunkName: "movieDetailsPage" */)
 );
 const ActorDetailsPage = lazy(() =>
-  import('./ActorDetailsPage' /* webpackChunkName: "actorDetailsPage" */)
+  import('../pages/ActorDetailsPage' /* webpackChunkName: "actorDetailsPage" */)
 );
 const ActorsMovies = lazy(() =>
   import('./ActorsMovies' /* webpackChunkName: "ActorsMovies" */)
@@ -43,25 +42,22 @@ export function App() {
           <Appbar theme={theme} changeTheme={changeTheme} />
           <Suspense fallback={<ThreeDots />}>
             <Routes>
-              <Route path="/" exact element={<HomePage />}></Route>
-              <Route path="/movies" element={<MoviesPage />}></Route>
-              <Route
-                path="/movies/:movieId/*"
-                element={<MovieDetailsPage />}
-              ></Route>
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
               <Route
                 path="/movies/:movieId/cast/actor/:actorId/*"
                 element={<ActorDetailsPage />}
-              ></Route>
+              />
               <Route
                 path="/moviesbyactor/:actorId/*"
                 element={<ActorsMovies />}
-              ></Route>
+              />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </Suspense>
         </Container>
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
       <ToastContainer />
     </ThemeProvider>
