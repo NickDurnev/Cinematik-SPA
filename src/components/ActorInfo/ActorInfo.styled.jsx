5718b8b04b455773ba82d9d1b2e11b019e5cd6ee
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-08-21 21:23:23 +0300

    fix: actorInfo styles

--- src/components/ActorInfo/ActorInfo.styled.jsx
+++ src/components/ActorInfo/ActorInfo.styled.jsx
@@ -2,25 +2,43 @@ import styled from '@emotion/styled';
 import { StyledLink } from 'components/MovieInfo/MovieInfo.styled';
 import { device } from 'deviceSizes';
 
-export const InfoContainer = styled.div`
-  display: flex;
+export const Container = styled.div`
+  display: block;
+  width: 100%;
   margin-top: 20px;
+  margin-bottom: 20px;
+
+  @media ${device.tablet} {
+    width: 93vw;
+    display: flex;
+    margin-top: 0;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: block;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  @media ${device.tablet} {
+    width: 93vw;
+    display: flex;
+    margin-top: 0;
+  }
 
   & img {
-    width: width: 85vw;
+    max-width: 365px;
+    margin: 0 auto;
     max-height: auto;
 
     @media ${device.tablet} {
       width: 250px;
     }
 
-     @media ${device.laptopL} {
+    @media ${device.laptopL} {
       width: 400px;
     }
-  }
-
-  @media ${device.mobileM} {
-    display: block;
   }
 `;
 
